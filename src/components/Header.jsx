190bc58d12b5d7ce5377d34ebfcfb07437c95c5f
sonle939 <lexuanson28102001@gmail.com>
2023-02-06 13:13:12 +0700

    search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,40 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import logo from "../assets/logo2.png";
-function Header({cart}) {
+import funituredata from '../data/funiture';
+
+function Header({ cart }) {
     const [fix, setFix] = useState(false);
+    const [data, setData] = useState(funituredata);
+    const [input, setInput] = useState(true);
     const [active, setActive] = useState(true);
+    const [search, setSearch] = useState('');
+    const suggest = [
+        {
+            id: 1,
+            name: "Chair"
+        },
+        {
+            id: 1,
+            name: "Table"
+        },
+        {
+            id: 1,
+            name: "Clock"
+        },
+        {
+            id: 1,
+            name: "Lamp"
+        },
+        {
+            id: 1,
+            name: "Mirror"
+        },
+    ]
     const fixHide = () => {
         if (window.scrollY >= 100) {
             setFix(true)
-        } else { 
+        } else {
             setFix(false)
         }
     }
@@ -18,13 +45,13 @@ function Header({cart}) {
                 <h3>welcome to leexs</h3>
                 <div className='header_img'>
                     <img src={logo} />
-                </div> 
+                </div>
                 <div className='header_icon'>
                     <p>Login</p>
                     <div className='header_icon_list'>
                         <Link to="/cart" className='header_icon_icon'>
-                            <i className="fa fa-shopping-cart" aria-hidden="true"></i> 
-                            {cart.length === 0 ?"(0)":<p>({cart.length})</p>}
+                            <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+                            {cart.length === 0 ? "(0)" : <p>({cart.length})</p>}
                         </Link>
                         <div className='header_icon_icon'>
                             <i className="fa fa-heart-o" aria-hidden="true"></i> (0)
@@ -32,7 +59,7 @@ function Header({cart}) {
                         <div className='header_icon_icon'>
                             <i className="fa fa-search" aria-hidden="true" onClick={() => setActive(!active)}></i>
                         </div>
-                    </div> 
+                    </div>
                 </div>
             </div>
             <ul className='header_bottom'>
@@ -42,9 +69,46 @@ function Header({cart}) {
                 <li><Link to="/aboutus" data-replace="About us"><span>About us</span></Link></li>
             </ul>
             <div className={active ? "overlay_search active" : "overlay_search"} >
-                <div className='overlay_input'>
-                    <input type="text" placeholder='Search entrie store here' />
+                <div className='search_container'>
+                    <h2>We can help you find ?</h2>
+                    <div className="input_wrapper">
                     <i className="fa fa-search" aria-hidden="true"></i>
+                        <input type="text"
+                            onChange={(e) => setSearch(e.target.value)}
+                            placeholder='Search entrie store here'
+                            name="title"
+                            value={search}
+                            onClick={()=>setInput(!input)}
+                        />
+                    </div> 
+                    <div className='suggest'>
+                        <p className='suggest_item' onClick={() => setSearch('table')}>
+                            Table
+                        </p>
+                        <p className='suggest_item' onClick={() => setSearch('chair')}>
+                            Chair
+                        </p>
+                        <p className='suggest_item' onClick={() => setSearch('mirror')}>
+                            Mirror
+                        </p>
+                        <p className='suggest_item' onClick={() => setSearch('lamp')}>
+                            Lamp
+                        </p>
+                        <p className='suggest_item' onClick={() => setSearch('c lock')}>
+                            Clock
+                        </p>
+                    </div>
+                </div>
+                <div className={search === "" ? "content_search" :"content_search active"}>
+                    {data.filter((item => {
+                        return item.title.toLowerCase().includes(search);
+                    })).map(item => (
+                        <div className='list_product'>
+                            {search === "" ? "" : (
+                                <p>{item.title}</p>
+                            )}
+                        </div>
+                    ))}
                 </div>
                 <div className='overlay-icon' onClick={() => setActive(!active)}>
                     <i className="fa fa-times-circle" aria-hidden="true"></i>
